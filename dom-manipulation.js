c03c92308b7d4248a3d1cc1f4e3bf0b5719528d7
Gaëlle Viso <gaelleviso@gmail.com>
2023-04-15 23:11:43 +0200

    Added button to change grid size and changed create grid so it deletes the previous one when a new grid is made

--- dom-manipulation.js
+++ dom-manipulation.js
@@ -3,21 +3,30 @@ const container = document.querySelector('.container')
 const div = document.createElement('div')
 div.setAttribute("class", "grid")
 
-function createGrid(){
+function createGrid(size){
+    while (container.firstChild) {
+        container.firstChild.remove()
+    }
     i = 0
-    while (i < 16*16) {
+    while (i < size*size) {
         let div_prime = div.cloneNode(true)
         container.appendChild(div_prime)
         i++
     }
 }
 
-createGrid()
+createGrid(16)
 
-const squares = document.querySelectorAll('.grid')
+const squares = document.querySelectorAll('.grid') 
 
 squares.forEach(square => {
     square.addEventListener('mouseover', function(){
         square.style.backgroundColor = 'black'
     })
+})
+
+const gridSizeButton = document.querySelector('button')
+gridSizeButton.addEventListener('click', function(){
+    let size = prompt('Please pick how many squares wide the grid should be')
+    createGrid(size)
 })
