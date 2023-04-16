2bd394737aeb9c1abbe5d387ec000694a4aa0954
Gaëlle Viso <gaelleviso@gmail.com>
2023-04-16 14:04:42 +0200

    Grid can change size with button and still be responsive

--- dom-manipulation.js
+++ dom-manipulation.js
@@ -4,6 +4,7 @@ const div = document.createElement('div')
 div.setAttribute("class", "grid")
 
 function createGrid(size){
+    container.style.gridTemplateColumns = "repeat(" + size + ", 1fr)";
     while (container.firstChild) {
         container.firstChild.remove()
     }
@@ -13,17 +14,19 @@ function createGrid(size){
         container.appendChild(div_prime)
         i++
     }
+    listenForMouseover()
 }
 
 createGrid(16)
 
-const squares = document.querySelectorAll('.grid') 
-
-squares.forEach(square => {
-    square.addEventListener('mouseover', function(){
-        square.style.backgroundColor = 'black'
+function listenForMouseover() {
+    const squares = document.querySelectorAll('.grid') 
+    squares.forEach(square => {
+        square.addEventListener('mouseover', function(){
+            square.style.backgroundColor = 'black'
+        })
     })
-})
+}
 
 const gridSizeButton = document.querySelector('button')
 gridSizeButton.addEventListener('click', function(){
