44da809ccaca175fcdd4b0529dbd31fc52663cc9
Gaëlle Viso <gaelleviso@gmail.com>
2023-04-16 19:50:30 +0200

    Create new file for mode, made code more structured

--- dom-manipulation.js
+++ dom-manipulation.js
@@ -6,60 +6,6 @@ div.setAttribute("class", "grid")
 const gridSizeButton = document.querySelector('button')
 gridSizeButton.addEventListener('click', changeGridSize)
 
-let greyModeActivated = true
-let rainbowModeActivated = false
-let eraserActivated = false
-let mouseoverActivated = true
-let lastClicked = 'grey'
-
-const greyMode = document.querySelector('#grey')
-greyMode.addEventListener('click', function(){
-    if (mouseoverActivated) {
-        greyModeActivated = true
-        rainbowModeActivated = false
-        eraserActivated = false
-    }
-    lastClicked = 'grey'
-})
-const rainbowMode = document.querySelector('#rainbow')
-rainbowMode.addEventListener('click', function(){
-    if (mouseoverActivated) {
-        if (mouseoverActivated) {
-            greyModeActivated = false
-            rainbowModeActivated = true
-            eraserActivated = false
-        }
-    }
-    lastClicked = 'rainbow'
-})
-
-const eraser = document.querySelector('#eraser')
-eraser.addEventListener('click', function(){
-    greyModeActivated = false
-    rainbowModeActivated = false
-    eraserActivated = true
-    lastClicked = 'eraser'
-})
-
-container.addEventListener('click', function(){
-    mouseoverActivated = !mouseoverActivated
-    if (!mouseoverActivated) {
-        greyModeActivated = false
-        rainbowModeActivated = false
-        eraserActivated = false
-    }
-    else {
-        if (lastClicked == 'grey') {
-            greyModeActivated = true
-        }
-        if (lastClicked == 'rainbow') {
-            rainbowModeActivated = true
-        }
-        if (lastClicked == 'eraser') {
-            eraserActivated = true
-        }
-    }
-})
 createGrid(16)
 
 function createGrid(size){
---
+++ drawing-modes.js
@@ -0,0 +1,54 @@
+let greyModeActivated = true
+let rainbowModeActivated = false
+let eraserActivated = false
+let mouseoverActivated = true
+let lastClicked = 'grey'
+
+const greyMode = document.querySelector('#grey')
+greyMode.addEventListener('click', function(){
+    if (mouseoverActivated) {
+        greyModeActivated = true
+        rainbowModeActivated = false
+        eraserActivated = false
+    }
+    lastClicked = 'grey'
+})
+const rainbowMode = document.querySelector('#rainbow')
+rainbowMode.addEventListener('click', function(){
+    if (mouseoverActivated) {
+        if (mouseoverActivated) {
+            greyModeActivated = false
+            rainbowModeActivated = true
+            eraserActivated = false
+        }
+    }
+    lastClicked = 'rainbow'
+})
+
+const eraser = document.querySelector('#eraser')
+eraser.addEventListener('click', function(){
+    greyModeActivated = false
+    rainbowModeActivated = false
+    eraserActivated = true
+    lastClicked = 'eraser'
+})
+
+container.addEventListener('click', function(){
+    mouseoverActivated = !mouseoverActivated
+    if (!mouseoverActivated) {
+        greyModeActivated = false
+        rainbowModeActivated = false
+        eraserActivated = false
+    }
+    else {
+        if (lastClicked == 'grey') {
+            greyModeActivated = true
+        }
+        if (lastClicked == 'rainbow') {
+            rainbowModeActivated = true
+        }
+        if (lastClicked == 'eraser') {
+            eraserActivated = true
+        }
+    }
+})
